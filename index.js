e1664498090a8e02eacb6e42d37f9c1973283df2
Shawn Wang <ixingo@126.com>
2021-05-24 17:12:56 +0800

    format code and small adjust

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let agree=document.querySelector("#agree")
-agree.onclick=function(){
-	alert("小姐姐，晚上下班一起走呗？")
+let agree = document.querySelector("#agree")
+agree.onclick = function () {
+	alert("就知道你要这么选！这双眼睛看透了太多～")
 }
 
-let disagree=document.querySelector("#disagree")
-disagree.onmouseenter=function(){
-	let x=Math.random()*600
-	let y=Math.random()*400
-	disagree.style.left=x+"px"
-	disagree.style.top=y+"px"
+let disagree = document.querySelector("#disagree")
+disagree.onmouseenter = function () {
+	let x = Math.random() * 600
+	let y = Math.random() * 400
+	disagree.style.left = x + "px"
+	disagree.style.top = y + "px"
 }
